7220aacdb19b617c152775fefbf383f18865210e
ben <ben@getbring.com>
2017-05-19 09:14:55 +0200

    Initial commit

---
+++ app/src/main/java/com/getbring/kotlin/TestDataClass.kt
@@ -0,0 +1,6 @@
+package com.getbring.kotlin
+
+/**
+ * Created by ben on 19.05.17.
+ */
+data class TestDataClass (val name:String)
---
+++ app/src/test/java/com/getbring/kotlin/Test.java
@@ -0,0 +1,13 @@
+package com.getbring.kotlin;
+
+/**
+ * Created by ben on 19.05.17.
+ */
+
+public class Test {
+
+    @org.junit.Test
+    public void test() {
+        TestDataClass testDataClass = new TestDataClass("");
+    }
+}
